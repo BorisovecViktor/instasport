9a1d475a7f2a09017d2af57e609a5c1785cef5cc
BorisovecViktor <viktorborisovets@gmail.com>
2020-06-22 09:45:53 +0300

    Updates

---
+++ precache-manifest.dc7248a05631aea7c0c2827c60349191.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c2fc8e35e837ca7f0ffab340a0fb3c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7f13786adfa7e8b6fdb6",
+    "url": "./static/css/main.817a7ac5.chunk.css"
+  },
+  {
+    "revision": "14243eb4c35737d425f2",
+    "url": "./static/js/2.9bfe12de.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.9bfe12de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f13786adfa7e8b6fdb6",
+    "url": "./static/js/main.2a6f759e.chunk.js"
+  },
+  {
+    "revision": "cd7566fc37b0e6b39b5e",
+    "url": "./static/js/runtime-main.24dcbce3.js"
+  },
+  {
+    "revision": "482e37260cd688576287f1976e958e0b",
+    "url": "./static/media/Mont-Bold.482e3726.otf"
+  },
+  {
+    "revision": "f0f845588eef439ac6ecc9b6ab42191c",
+    "url": "./static/media/Mont-Regular.f0f84558.otf"
+  },
+  {
+    "revision": "d41a520b1802421f24e21279736e1871",
+    "url": "./static/media/Mont-SemiBold.d41a520b.otf"
+  },
+  {
+    "revision": "b96cc7d79bf73bf4f0673de0bd10c044",
+    "url": "./static/media/arrow-down.b96cc7d7.svg"
+  },
+  {
+    "revision": "5ca5c1f58aa3bcefb2bf88439be8fe51",
+    "url": "./static/media/arrow-left.5ca5c1f5.svg"
+  },
+  {
+    "revision": "c04f58ba855a95a0fea25837001a3e4e",
+    "url": "./static/media/breadcrumb-arrow.c04f58ba.svg"
+  },
+  {
+    "revision": "bbe701a798719d996b7e9325607159d2",
+    "url": "./static/media/breadcrumb-home.bbe701a7.svg"
+  },
+  {
+    "revision": "6da4f5192e45db4de6412d3569a2a464",
+    "url": "./static/media/footer-top-arrow.6da4f519.svg"
+  },
+  {
+    "revision": "fc2574b435c86fbc0ecd9c51b90be43b",
+    "url": "./static/media/goback-arrow.fc2574b4.svg"
+  },
+  {
+    "revision": "d5f941ebd8ca6134fbbbae1e936f6a34",
+    "url": "./static/media/sad-cry.d5f941eb.svg"
+  }
+]);
